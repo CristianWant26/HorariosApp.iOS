92ea24d00936ea3fcc12b86a48b2660aa733530e
Cristian Gómez <yair.gomher26@gmail.com>
2024-08-30 13:47:53 -0600

    Initial Commit

---
+++ HorariosApp.iOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HorariosApp.iOS
+//
+//  Created by Cristian Gómez  on 30/08/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HorariosApp.iOS/HorariosApp_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  HorariosApp_iOSApp.swift
+//  HorariosApp.iOS
+//
+//  Created by Cristian Gómez  on 30/08/24.
+//
+
+import SwiftUI
+
+@main
+struct HorariosApp_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
