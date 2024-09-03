d119af7d1d661414eb9b1f41187af06aa31726fe
Cristian Gómez <yair.gomher26@gmail.com>
2024-09-03 14:41:38 -0600

    Comentario del inicio de Git-Hub

--- HorariosApp.iOS/ContentView.swift
+++ HorariosApp.iOS/ContentView.swift
@@ -10,12 +10,42 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+            HStack{
+                
+                Rectangle()
+                    .foregroundColor(.black)
+                Rectangle()
+                    .foregroundColor(.yellow)
+                Rectangle()
+                    .foregroundColor(.cyan)
+                
+            } .frame(height:100)
+            Rectangle()
+                .frame(height:100)
+                .foregroundColor(.orange)
+            HStack{
+                Circle()
+                    .frame(width:100)
+                Rectangle()
+                    .frame(width: 80, height: 200)
+                Circle()
+                    .frame(width:100)
+            }
+            Rectangle()
+                .foregroundColor(.orange)
+                .frame(height:100)
+            HStack{
+                Rectangle()
+                    .frame(height:200)
+                    .foregroundColor(.blue)
+                Rectangle()
+                    .frame(height: 200)
+                    .foregroundColor(.green)
+                Rectangle()
+                    .frame(height: 200)
+                    .foregroundColor(.yellow)
+            }
+        }.background(.red)
     }
 }
 
